8fab0f02636a1efb645dc5d2355df877ae5539c0
Archit Jain <architjain798@gmail.com>
2022-04-11 14:04:11 +0530

    fixed the cors issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectToMongoDB = require("./db");
 const apiRoutes = require("./routes/apiRoutes");
+let cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ connectToMongoDB();
 
 //built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.status(200).send("Hello server is running").end();
